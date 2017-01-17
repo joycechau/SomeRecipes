881cf4bfd73f7f95b14ac938ffedae9f93588a1a
Joyce Chau <jyc.chau@gmail.com>
2017-01-17 00:13:26 -0800

    Add styling to recipe form

--- frontend/components/recipe/new_recipe_form.jsx
+++ frontend/components/recipe/new_recipe_form.jsx
@@ -46,62 +46,88 @@ class NewRecipeForm extends React.Component {
 
   render() {
     return (
-      <div className="recipe-form">
+      <div className="recipe-form-container">
         <h2 className="recipe-form-header">
           Add Recipe
         </h2>
-        <form onSubmit={this.handleSubmit}>
-          <label className="recipe-form-label">Title
+        <form onSubmit={this.handleSubmit}
+              className="recipe-form">
+          <label className="recipe-form-label">
+            <h2 className="recipe-form-label-description">
+              Title
+            </h2>
             <input type="text"
               value={this.state.title}
               onChange={this.update('title')}
               className="recipe-form-input-title"/>
           </label>
-          <br />
-          <label className="recipe-form-label">Category
-            <select onChange={this.update('category')}>
+          <label className="recipe-form-label-category">
+            <h2 className="recipe-form-label-description">
+              Category
+            </h2>
+            <select onChange={this.update('category')}
+                    className="recipe-form-select">
               <option value="--Select Category--"
                       disabled="true"
-                      selected="true">
+                      selected="true"
+                      className="recipe-form-disabed-option">
                       --Select Category --
               </option>
-              <option value="Appetizers">Appetizers</option>
-              <option value="Main Dishes">Main Dishes</option>
-              <option value="Desserts">Desserts</option>
-              <option value="Breakfast">Breakfast</option>
-              <option value="Sides">Sides</option>
-              <option value="Other">Other
+              <option value="Appetizers" className="recipe-form-option">Appetizers</option>
+              <option value="Main Dishes" className="recipe-form-option">Main Dishes</option>
+              <option value="Desserts" className="recipe-form-option">Desserts</option>
+              <option value="Breakfast" className="recipe-form-option">Breakfast</option>
+              <option value="Sides" className="recipe-form-option">Sides</option>
+              <option value="Other" className="recipe-form-option">Other
               </option>
             </select>
           </label>
-          <br/>
-          <label className="recipe-form-label"> Description
+          <label className="recipe-form-label">
+            <h2 className="recipe-form-label-description">
+              Description
+            </h2>
             <textarea
                    value={this.state.description}
                    onChange={this.update('description')}
                    className="recipe-form-input-description">
             </textarea>
           </label>
-          <br/>
-          <label className="recipe-form-label"> Ingredients
+          <label className="recipe-form-label">
+            <h2 className="recipe-form-label-description">
+              Ingredients
+            </h2>
             <textarea
                    value={this.state.ingredients}
                    onChange={this.update('ingredients')}
+                   placeholder="Put each ingredient on its own line."
                    className="recipe-form-input-ingredients">
             </textarea>
           </label>
-          <br/>
-          <label className="recipe-form-label"> Directions
+          <label className="recipe-form-label">
+            <h2 className="recipe-form-label-description">
+              Directions
+            </h2>
             <textarea
                    value={this.state.directions}
                    onChange={this.update('directions')}
-                   className="recipe-form-input-directions">
+                   className="recipe-form-input-directions"
+                   placeholder="Put each step on its own line.">
             </textarea>
           </label>
-          <br/>
-          <input type="submit"
-            value="Submit"
-            className="recipe-form-submit-button"/>
+          <label className="recipe-form-label">
+            <h2 className="recipe-form-label-description">
+              Image URL
+            </h2>
+            <input type="text"
+              value={this.state.image_url}
+              onChange={this.update('image_url')}
+              className="recipe-form-input-image_url"/>
+          </label>
+          <label className="recipe-form-label">
+            <input type="submit"
+              value="Submit"
+              className="recipe-form-submit-button"/>
+          </label>
           <h2>{this.errorText()}</h2>
         </form>
       </div>
